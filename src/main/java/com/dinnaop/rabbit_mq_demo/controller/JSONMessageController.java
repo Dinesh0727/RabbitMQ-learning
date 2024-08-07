fbf78f3e56904a36640207dcc61398069fef51a9
Dinesh <dineshttt2003@gmail.com>
2024-08-07 10:33:51 +0530

    Changed the api response text

--- src/main/java/com/dinnaop/rabbit_mq_demo/controller/JSONMessageController.java
+++ src/main/java/com/dinnaop/rabbit_mq_demo/controller/JSONMessageController.java
@@ -8,8 +8,6 @@ import com.dinnaop.rabbit_mq_demo.dto.User;
 import com.dinnaop.rabbit_mq_demo.publisher.RabbitMQJSONProducer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,7 +23,7 @@ public class JSONMessageController {
     public ResponseEntity<String> sendJSONMessage(@RequestBody User user) throws Exception {
         rabbitMQJSONProducer.sendJSONMessage(user);
 
-        return ResponseEntity.ok("Send JSON RabbitMQ Message : " + user.toString());
+        return ResponseEntity.ok("Send JSON RabbitMQ Message - User : " + user.toString());
     }
 
 }
